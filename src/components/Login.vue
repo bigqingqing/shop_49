ba7925a715fcf87f99f3d5289c0b02192e13c750
bigqingqing <1842290287@qq.com>
2019-10-15 21:19:08 +0800

    Login is scusses

---
+++ src/components/Login.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="login">
+    <img src="../assets/avatar.jpg" alt />
+    <el-form
+      ref="form"
+      :model="form"
+      label-width="80px"
+      :label-position="labelPosition"
+      :rules="rules"
+      status-icon
+    >
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="btn-r" @click="login" type="primary">登录</el-button>
+        <el-button @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      labelPosition: 'left',
+      rules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 3,
+            max: 12,
+            message: '长度在 3 到 12 位的用户名',
+            trigger: ['blur', 'change']
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 3,
+            max: 12,
+            message: '长度在 3 到 12 位的密码',
+            trigger: ['blur', 'change']
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset () {
+      console.log(1)
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate((isValid, obj) => {
+        // isValid 是否通过校验 obj 未通过校验的信息
+        if (!isValid) return
+        // 通过校验了, 该发ajax了
+        axios({
+          method: 'post',
+          url: 'http://localhost:8888/api/private/v1/login',
+          data: this.form
+        }).then(res => {
+          console.log(res)
+          // eslint-disable-next-line no-unused-vars
+          const { meta, data } = res.data // 解构出data
+          localStorage.setItem('token', data.token) // 将data里的token拿出来存在localStorage
+          if (meta.status === 200) {
+            this.$message({
+              message: meta.msg,
+              type: 'success'
+            })
+            this.$router.push('/Index')
+          } else {
+            this.$message({
+              message: meta.msg,
+              type: 'error'
+            })
+          }
+        })
+        // console.log('发ajax')
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+.login {
+  height: 100%;
+  width: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  position: relative;
+  img {
+    border-radius: 50%;
+    border: 5px solid #fff;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 70px;
+  }
+  .el-form {
+    width: 400px;
+    background-color: #fff;
+    margin: 200px auto;
+    border-radius: 20px;
+    padding: 75px 30px 25px;
+    .btn-r {
+      margin-right: 120px;
+    }
+  }
+}
+</style>
